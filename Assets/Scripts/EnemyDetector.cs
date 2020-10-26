f6ef07ae830768ea1f930d5e08c0f162250514a4
LuVe96 <LukiVe16@gmail.com>
2020-10-26 20:22:03 +0100

    implemented EnemyDetection

---
+++ Assets/Scripts/EnemyDetector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+
+    public ArrayList enemys { get; private set; } = new ArrayList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            enemys.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            enemys.Remove(other.gameObject);
+        }
+    }
+
+    public void removeFromEnemyList(GameObject enemy)
+    {
+        if (enemy.tag == "Enemy")
+        {
+            enemys.Remove(enemy);
+        }
+    }
+
+
+}
