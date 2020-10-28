dfe4455564eef5fb75b6801d09279623c16f7a6e
LuVe96 <LukiVe16@gmail.com>
2020-10-28 11:30:27 +0100

    add lifebar for enemy

---
+++ Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+
+    public float lifeAmount = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Enemy Collider:  " + collision.transform.name);
+    }
+}
