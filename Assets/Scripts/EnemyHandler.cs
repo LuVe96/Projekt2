739ec339b3967dc69c59eb014d986f813a03335b
LuVe96 <LukiVe16@gmail.com>
2020-10-28 21:28:49 +0100

    let enemy die

--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHandler : MonoBehaviour
 {
 
     public float lifeAmount = 100;
+    private float FullLifeAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        FullLifeAmount = lifeAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(lifeAmount <= 0)
+        {
+            GameObject.Find("EnemyDetection").GetComponent<EnemyDetector>().removeFromEnemyList(transform.Find("Charakter").gameObject);
+            Destroy(gameObject);
+        }
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Enemy Collider:  " + collision.transform.name);
+        if (collision.transform.tag == "Arrow")
+        {
+            lifeAmount -= collision.gameObject.GetComponent<ArrowHandler>().damage;
+
+            transform.Find("Canvas/lifebar/front").gameObject.GetComponent<Image>().fillAmount = lifeAmount / FullLifeAmount;
+        }
     }
 }
