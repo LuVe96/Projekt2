c53b37971dd1a3b7b2c5bd4ebbcd3b7f9bf17f4f
LuVe96 <LukiVe16@gmail.com>
2020-12-01 15:54:31 +0100

    added Inventory script

---
+++ Assets/Scripts/Loot/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static Inventory Instance = null;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            //DontDestroyOnLoad(gameObject);  // the Singelton Obj gets not deleted when change szene
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public List<LootItem> items = new List<LootItem>();
+
+    public void Add(LootItem item) {
+        items.Add(item);
+    }
+
+    public void Remove(LootItem item)
+    {
+        items.Remove(item);
+    }
+}
