26302391e669551951712fa1b0abc3c110fcd490
LuVe96 <LukiVe16@gmail.com>
2021-05-20 15:30:45 +0200

    Add Nodes with right click

--- Assets/QuestSystem/Editor/QuestEditorWindow.cs
+++ Assets/QuestSystem/Editor/QuestEditorWindow.cs
@@ -10,6 +10,9 @@ namespace QuestSystem.Quest
     {
         static Quest currentQuest = null;
 
+        private List<Node> nodes;
+        private GUIStyle nodeStyle;
+
         [MenuItem("Tools/QuestWindow")]
         public static void Init()
         {
@@ -20,6 +23,26 @@ namespace QuestSystem.Quest
         {
             Init();
             currentQuest = (Quest)target;
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+
+            nodeStyle = new GUIStyle();
+            nodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1.png") as Texture2D;
+            nodeStyle.border = new RectOffset(12, 12, 12, 12);
+        }
+
+        private void OnSelectionChanged()
+        {
+            Quest quest = Selection.activeGameObject.GetComponent<Quest>() as Quest;
+            Debug.Log("Selectd: " + Selection.activeGameObject);
+            if (quest != null)
+            {
+                currentQuest = quest;
+                Repaint();
+            }
         }
 
         private void OnGUI()
@@ -34,25 +57,53 @@ namespace QuestSystem.Quest
                 currentQuest.questName = EditorGUILayout.TextField("Name: ", currentQuest.questName);
             }
 
+            DrawNodes();
 
+            ProccessEvents(Event.current);
 
+            if (GUI.changed) Repaint();
         }
 
-        private void OnEnable()
+        private void ProccessEvents(Event current)
         {
-            Selection.selectionChanged += OnSelectionChanged;
-
-        }
-
-        private void OnSelectionChanged()
-        {
-            Quest quest = Selection.activeGameObject.GetComponent<Quest>() as Quest;
-            Debug.Log("Selectd: " + Selection.activeGameObject);
-            if (quest != null)
+            switch (current.type)
             {
-                currentQuest = quest;
-                Repaint();
+                case EventType.MouseDown:
+                    if (current.button == 1)
+                    {
+                        ProcessContextMenu(current.mousePosition);
+                    }
+                    break;
             }
         }
+
+        private void ProcessContextMenu(Vector2 mousePosition)
+        {
+            GenericMenu genericMenu = new GenericMenu();
+            genericMenu.AddItem(new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition));
+            genericMenu.ShowAsContext();
+        }
+
+        private void OnClickAddNode(Vector2 mousePosition)
+        {
+            if (nodes == null)
+            {
+                nodes = new List<Node>();
+            }
+
+            nodes.Add(new Node(mousePosition, 200, 50, nodeStyle));
+        }
+
+        private void DrawNodes()
+        {
+            if (nodes != null)
+            {
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    nodes[i].Draw();
+                }
+            }
+        }
+
     } 
 }
