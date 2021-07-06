6c00428a224ab79b9b14246318b3b148060b1ba6
LuVe96 <LukiVe16@gmail.com>
2021-07-06 18:39:22 +0200

    implement QuestLogManager

--- Assets/QuestSystem/Components/QuestLogManager.cs
+++ Assets/QuestSystem/Components/QuestLogManager.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using QuestSystem.Quest;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace QuestSystem
 {
@@ -8,7 +11,17 @@ namespace QuestSystem
     {
 
         [SerializeField] GameObject logPager;
+        [SerializeField] Transform activeLogs;
+        [SerializeField] Transform completedLogs;
+
         [SerializeField] GameObject detailsPage;
+        [SerializeField] Transform detailText;
+
+        [SerializeField] GameObject questLogButtonPrefab;
+
+        [SerializeField] GameObject questLogTextPrefab;
+
+        [SerializeField] Dictionary<string, Logs> questLogs = new Dictionary<string,Logs>(); //TODO: savable
 
 
 
@@ -18,9 +31,67 @@ namespace QuestSystem
 
         }
 
+        public void AddQuestLog(string questLogName,  QuestState state, string logText)
+        {
+            if (!questLogs.ContainsKey(questLogName))
+            { 
+                questLogs.Add(questLogName, new Logs(state));
+            }
+
+            questLogs[questLogName].LogTexts.Add(logText);
+        }
+
+        private void setupQuestLog()
+        {
+            ClearChildren(activeLogs);
+            ClearChildren(completedLogs);
+
+            foreach (var log in questLogs)
+            {
+                GameObject newLog = null;
+                switch (log.Value.State)
+                {
+                    case QuestState.Inactive:
+                        break;
+                    case QuestState.Active:
+                        newLog = Instantiate(questLogButtonPrefab, activeLogs);
+                        break;
+                    case QuestState.Failed:
+                    case QuestState.Passed:
+                        newLog = Instantiate(questLogButtonPrefab, completedLogs);
+                        break;
+                    default:
+                        break;
+                }
+                if(newLog != null)
+                {
+                    newLog.GetComponent<Button>().onClick.AddListener(delegate { OnClickLog(log.Key); });
+                    newLog.transform.GetChild(0).GetComponent<Text>().text = log.Key;
+                }
+            }
+        }
+
+        private void OnClickLog(string key)
+        {
+            foreach (string logTxt in questLogs[key].LogTexts)
+            {
+                GameObject newTxt = Instantiate(questLogTextPrefab, detailText);
+                newTxt.transform.GetChild(0).GetComponent<Text>().text = logTxt;
+            }
+        }
+
+        private void ClearChildren(Transform transform)
+        {
+            foreach (Transform item in transform)
+            {
+                Destroy(item.gameObject);
+            }
+        }
+
         public void OpenLogPager()
         {
             logPager.SetActive(true);
+            setupQuestLog();
         }
 
         public void CloseLogDetails()
@@ -28,6 +99,22 @@ namespace QuestSystem
             detailsPage.SetActive(false);
         }
 
+    }
+
+    public class Logs
+    {
+        [SerializeField] QuestState state;
+        [SerializeField] List<string> logTexts = new List<string>();
+
+        public Logs(QuestState state, List<string> logTexts = null)
+        {
+            this.state = state;
+            if( logTexts != null)
+                this.logTexts = logTexts;
+        }
+
+        public QuestState State { get => state; set => state = value; }
+        public List<string> LogTexts { get => logTexts; set => logTexts = value; }
     }
 
 }
