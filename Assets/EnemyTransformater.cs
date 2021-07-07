01406ea30bfafb13d803be3462da2cde8e8e3647
LuVe96 <LukiVe16@gmail.com>
2021-07-07 23:29:58 +0200

    create enemyTransformer

---
+++ Assets/EnemyTransformater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTransformater : MonoBehaviour
+{
+
+    [SerializeField] MonoBehaviour[] monoBehaviours;
+
+    public void TransormToEnemy()
+    {
+        foreach (MonoBehaviour mono in monoBehaviours)
+        {
+            mono.enabled = true;
+        }
+
+        gameObject.tag = "Enemy";
+        GameObject.Find("Player").GetComponentInChildren<EnemyDetector>().AddEnemyToList(gameObject);
+    }
+}
