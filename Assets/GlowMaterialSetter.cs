9a3fa277e8ee474a116a56e48ae3102ae818e3bb
LuVe96 <LukiVe16@gmail.com>
2021-07-10 22:18:03 +0200

    add fantasy characters, use them as npc

--- Assets/GlowMaterialSetter.cs
+++ Assets/GlowMaterialSetter.cs
@@ -16,9 +16,9 @@ public class GlowMaterialSetter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        outline.enabled = false;
         skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
         stdMaterial = skinnedMeshRenderer.material;
-        outline.enabled = false;
     }
 
    public void ChangeGlowMaterial(bool glow)
